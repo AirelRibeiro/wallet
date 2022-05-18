0783227c66dd6232b2d3315d3583c1667a61df32
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-05-18 18:47:54 -0300

    Implementação de reducer wallet e importação do mesmo no index.js para combina-lo com o reducer user no estago geral

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import wallet from './wallet';
 // import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
-const reducer = combineReducers({ user });
+const reducer = combineReducers({ user, wallet });
 
 export default reducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { ACTION_SAVE_CURRENCIES, GET_CURRENCIES } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  loading: false,
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case GET_CURRENCIES:
+    return { ...state, loading: true };
+  case ACTION_SAVE_CURRENCIES:
+    console.log('passei por aqui');
+    return { ...state, currencies: action.currencies, loading: false };
+  default:
+    return { ...state };
+  }
+};
+
+export default wallet;
