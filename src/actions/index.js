77cb2232186510b5bd94410a4ba775cad6b7c1c9
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-05-18 18:59:48 -0300

    Criação de actions para requisição em Api e armazenamento no estado central

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,34 @@
-// Coloque aqui suas actions
+import apiRequest from '../apiRequest';
+
 const ACTION_LOGIN = 'ACTION_LOGIN';
+const GET_CURRENCIES = 'GET_CURRENCIES';
+const ACTION_SAVE_CURRENCIES = 'ACTION_SAVE_CURRENCIES';
 
 const actionLogin = (email) => ({
   type: ACTION_LOGIN,
   email,
 });
 
+const requestCurrencies = () => ({ type: GET_CURRENCIES });
+
+const actionSaveCurrencies = (currencies) => ({
+  type: ACTION_SAVE_CURRENCIES,
+  currencies,
+});
+
+const fetchCurrencies = () => async (dispatch) => {
+  const currenciesData = await apiRequest();
+  const currenciesAbbreviations = Object.keys(currenciesData);
+  const currenciesAbbreviationsFilter = currenciesAbbreviations
+    .filter((coin) => coin !== 'USDT');
+  dispatch(actionSaveCurrencies(currenciesAbbreviationsFilter));
+};
+
 export {
   actionLogin,
   ACTION_LOGIN,
+  requestCurrencies,
+  GET_CURRENCIES,
+  fetchCurrencies,
+  ACTION_SAVE_CURRENCIES,
 };
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { saveCurrencies } = this.props;
+    saveCurrencies();
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <section>
+        <header>
+          <h1 data-testid="email-field">{ email }</h1>
+        </header>
+        <h3 data-testid="header-currency-field">BRL</h3>
+        <h2 data-testid="total-field">0</h2>
+      </section>
+
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  saveCurrencies: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveCurrencies: () => dispatch(fetchCurrencies()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
