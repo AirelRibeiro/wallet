2e87baffbf3f1d7e82a448da7ea80b898843bd5c
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-05-18 23:26:59 -0300

    Criação do componente ExpenseTable com campos pedidos pelo requisito 07

---
+++ src/components/ExpenseTable.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ExpenseTable extends React.Component {
+  render() {
+    return (
+      <tr>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </tr>
+    );
+  }
+}
+
+export default ExpenseTable;
