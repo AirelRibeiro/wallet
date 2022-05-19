b256256ac01dc001684b11d561bccadc8f9aff70
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-05-18 23:03:07 -0300

    Implementação da actionSaveExpense para salvar o objeto correspondente a despesa corrente no estado geral sob a chave expenses

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import apiRequest from '../apiRequest';
 const ACTION_LOGIN = 'ACTION_LOGIN';
 const GET_CURRENCIES = 'GET_CURRENCIES';
 const ACTION_SAVE_CURRENCIES = 'ACTION_SAVE_CURRENCIES';
+const ACTION_SAVE_EXPENSE = 'ACTION_SAVE_EXPENSE';
 
 const actionLogin = (email) => ({
   type: ACTION_LOGIN,
@@ -14,6 +15,11 @@ const requestCurrencies = () => ({ type: GET_CURRENCIES });
 const actionSaveCurrencies = (currencies) => ({
   type: ACTION_SAVE_CURRENCIES,
   currencies,
+});
+
+const actionSaveExpense = (expense) => ({
+  type: ACTION_SAVE_EXPENSE,
+  expense,
 });
 
 const fetchCurrencies = () => async (dispatch) => {
@@ -31,4 +37,6 @@ export {
   GET_CURRENCIES,
   fetchCurrencies,
   ACTION_SAVE_CURRENCIES,
+  actionSaveExpense,
+  ACTION_SAVE_EXPENSE,
 };
